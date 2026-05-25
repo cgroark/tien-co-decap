c849ca47f3e0563a63d351cb32ebd4e7944de917
Colin Groark <colin@nexleaf.org>
2026-05-25 20:56:03 +0100

    deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: '',
+  site: 'https://your-site-name.netlify.app',
   integrations: [react()],
 });
